070c66d064ad04a05200a2288761e27465c6f367
Garreth Piekarski <garreth@Tests-MacBook-Pro.local>
2016-10-13 16:23:48 -0400

    added basic csv and text support

---
+++ appjs/application.js
@@ -0,0 +1,3 @@
+var app = angular.module('simplefeed', []);
+
+app.controller('FeedController', ['$scope', function($scope){}]);
--- appjs/ui.js
+++ appjs/ui.js
@@ -3,11 +3,16 @@
     var sourceUrl = $.trim($('#urlField').val()),
         sourceType = $('.sourceTypeSelect:checked').val(),
         feedFormat = $('.feedFormatSelect:checked').val();
+    $('#viewPanel').html('Loading new data. <strong>This may take a while.</strong>');
     $.ajax({
       url: './urldata?url=' + sourceUrl + "&sourceType=" + sourceType + "&feedFormat=" + feedFormat,
       dataType: 'json',
       success: function(data) {
-        $('#viewPanel').html(JSON.stringify(data));
+        if (data.length) {
+          $('#viewPanel').text(JSON.stringify(data));
+        } else {
+          $('#viewPanel').text('No data was returned. Check your settings.');
+        }
       }
     });
   });
